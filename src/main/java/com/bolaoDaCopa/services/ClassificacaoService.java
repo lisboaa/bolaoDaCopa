cfb9b44980a5f468bceec28a14a63e40297d4c9b
Doug Alves <doug-pr@hotmail.com>
2022-11-29 22:38:48 -0300

    Implementando a classe Classificação

---
+++ src/main/java/com/bolaoDaCopa/services/ClassificacaoService.java
@@ -0,0 +1,12 @@
+package com.bolaoDaCopa.services;
+
+import com.bolaoDaCopa.models.Classificacao;
+
+import java.util.List;
+
+public interface ClassificacaoService {
+    List<Classificacao> listar();
+    Classificacao editar(Classificacao classificacao);
+    Classificacao salvar(Classificacao classificacao);
+    void deletar(Long id_classificacao);
+}
