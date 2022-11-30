bdc98bcf45ad300db87cc22b708dc00532fa2b9f
thamires.6966 <thamires.6966@aluno.pr.senac.br>
2022-11-29 22:41:57 -0300

    Criação_Endereço

---
+++ src/main/java/com/example/bolaoDaCopa/BolaoDaCopaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolaoDaCopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoDaCopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoDaCopaApplication.class, args);
+	}
+
+}
