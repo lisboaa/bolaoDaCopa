97bd3cfdb1a7e7291566bb3e6e8761c26df07825
KAYSILVHA <kaysilvha@gmail.com>
2022-12-06 20:21:56 -0300

    "bolaoDaCopa"

--- src/main/java/com/example/bolaoDaCopa/models/Usuario.java
+++ src/main/java/com/example/bolaoDaCopa/models/Usuario.java
@@ -35,14 +35,14 @@ public class Usuario {
     @JoinTable(name = "usuario_documento",
             joinColumns = @JoinColumn(name = "id_usuario"),
             inverseJoinColumns = @JoinColumn(name = "id_documento"))
-    private com.example.BolaoDaCopa.models.Documento documento;
+    private Documento documento;
 
 
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH, CascadeType.PERSIST}, fetch = FetchType.EAGER)
     @JoinTable(name = "usuario_endereco",
             joinColumns = @JoinColumn(name = "id_usuario"),
             inverseJoinColumns = @JoinColumn(name = "id_endereco"))
-    private com.example.bolaoDaCopa.models.Endereco endereco;
+    private Endereco endereco;
 
 
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH, CascadeType.PERSIST}, fetch = FetchType.EAGER)
